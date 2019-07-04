f291c9aad585f587807ae1aa1aee4e3883389681
Erik Kimsey <kimseyerik@gmail.com>
2019-07-04 19:12:47 -0400

    Completed building out camera toolbar

--- components/cameraToolbar.js
+++ components/cameraToolbar.js
@@ -19,36 +19,38 @@ export default ({
 	onCaptureOut,
 	onShortCapture
 }) => (
-	<Grid>
+	<Grid style={styles.bottomToolbar}>
 		<Row>
-			<Col>
+			<Col style={styles.alignCenter}>
 				<TouchableOpacity
 					onPress={() =>
 						setFlashMode(flashMode === CameraFlashModes.on ? CameraFlashModes.off : CameraFlashModes.on)}
 				>
-					<Ionicons />
+					<Ionicons
+						name={flashMode === CameraFlashModes.on ? 'md-flash' : 'md-flash-off'}
+						color="white"
+						size={30}
+					/>
 				</TouchableOpacity>
 			</Col>
-			<Col>
+			<Col size={2} style={styles.alignCenter}>
 				<TouchableWithoutFeedback
 					onPressIn={onCaptureIn}
 					onPressOut={onCaptureOut}
 					onPressIn={onCaptureIn}
 					onPress={onShortCapture}
 				>
-					<View style={[ styles.captureBtn, capturing && style.captureBtnActive ]}>
+					<View style={[ styles.captureBtn, capturing && styles.captureBtnActive ]}>
 						{capturing && <View style={styles.captureBtnInternal} />}
 					</View>
-					>
-					<Ionicons />
 				</TouchableWithoutFeedback>
 			</Col>
-			<Col>
+			<Col style={styles.alignCenter}>
 				<TouchableOpacity
 					onPress={() =>
-						setFlashMode(flashMode === CameraFlashModes.on ? CameraFlashModes.off : CameraFlashModes.on)}
+						setCameraType(cameraType === CameraTypes.back ? CameraTypes.front : CameraTypes.back)}
 				>
-					<Ionicons />
+					<Ionicons name="md-reverse-camera" color="white" size={30} />
 				</TouchableOpacity>
 			</Col>
 		</Row>
